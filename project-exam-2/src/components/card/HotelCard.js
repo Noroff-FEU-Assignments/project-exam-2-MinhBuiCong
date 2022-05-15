df534d8562372fa031b294f246e60135b1a826d1
MinhBuiCong <62849019+MinhBuiCong@users.noreply.github.com>
2022-05-15 23:01:27 +0200

    card style done

--- project-exam-2/src/components/card/HotelCard.js
+++ project-exam-2/src/components/card/HotelCard.js
@@ -5,14 +5,12 @@ import Button from "react-bootstrap/Button";
 function HotelCard() {
   return (
     <main className="card-container">
-      <Card className="card" style={{ width: "20rem" }}>
+      <Card className="card">
         <Card.Img className="card-image" src="holder.js/100px180" />
         <Card.Body className="card-text-container">
           <Card.Title className="card-title">Card Title</Card.Title>
-          <Card.Text className="card-description">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
+          <Card.Text className="card-subtitle">Location</Card.Text>
+          <Card.Text className="card-description">Description...</Card.Text>
           <Button className="card-button">Go somewhere</Button>
         </Card.Body>
       </Card>
