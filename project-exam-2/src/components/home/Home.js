bec1f6a65d173858f97e18c11595240c7d1b1c2b
MinhBuiCong <62849019+MinhBuiCong@users.noreply.github.com>
2022-05-15 20:17:13 +0200

    update

--- project-exam-2/src/components/home/Home.js
+++ project-exam-2/src/components/home/Home.js
@@ -4,10 +4,12 @@ import React from "react";
 import Search from "../search/Search";
 import HotelCard from "../card/HotelCard";
 import Footer from "./components/Footer";
+import { url } from "../../constants/api";
 
 // const FEATURED_PLACE_INDEX = 1;
 
 function Home() {
+  console.log(url);
   return (
     <main>
       <Search />
