9c3e514ee3c8881829bc458b28bda92b8702c313
MinhBuiCong <62849019+MinhBuiCong@users.noreply.github.com>
2022-05-16 00:15:06 +0200

    navbar styling

--- project-exam-2/src/components/layout/Nav.js
+++ project-exam-2/src/components/layout/Nav.js
@@ -3,31 +3,44 @@ import { NavLink } from "react-router-dom";
 import NavBar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 
 function Navbar() {
   return (
-    <NavBar className="navbar" expand={false}>
+    <NavBar className="navbar navbar-dark" expand={false}>
       <Container className="container">
         <NavBar.Brand className="navbar-brand" href="/">
           Holidaze
         </NavBar.Brand>
-        <NavBar.Toggle aria-controls="basic-navbar-nav" />
-        <NavBar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <NavLink className="link" to="/">
-              Home
-            </NavLink>
-            <NavLink className="link" to="/hotels">
-              Hotels
-            </NavLink>
-            <NavLink className="link" to="/admin">
-              Admin
-            </NavLink>
-            <NavLink className="link" to="/contact">
-              Contact
-            </NavLink>
-          </Nav>
-        </NavBar.Collapse>
+        <div className="button-container">
+          <Button className="login-button">login</Button>
+          <Button variant="danger" className="logout-button">
+            log out
+          </Button>
+          <NavBar.Toggle
+            className="navbar-burger"
+            aria-controls="basic-navbar-nav"
+          />
+          <NavBar.Collapse
+            className="navbar-dropdown-container"
+            id="basic-navbar-nav"
+          >
+            <Nav className="me-auto">
+              <NavLink className="link" to="/">
+                Home
+              </NavLink>
+              <NavLink className="link" to="/hotels">
+                Hotels
+              </NavLink>
+              <NavLink className="link admin-link" to="/admin">
+                Admin
+              </NavLink>
+              <NavLink className="link" to="/contact">
+                Contact
+              </NavLink>
+            </Nav>
+          </NavBar.Collapse>
+        </div>
       </Container>
     </NavBar>
   );
