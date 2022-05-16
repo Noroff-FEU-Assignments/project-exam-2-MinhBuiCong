bfa06b807400a5d926d79b3374900e41d67343aa
MinhBuiCong <62849019+MinhBuiCong@users.noreply.github.com>
2022-05-17 00:11:18 +0200

    update card

--- project-exam-2/src/components/card/HotelCard.js
+++ project-exam-2/src/components/card/HotelCard.js
@@ -6,17 +6,19 @@ import image from "../../images/image-1.jpg";
 function HotelCard() {
   return (
     <main className="card-container">
-      <Card className="card">
-        <Card.Img className="card-image" src={image} alt="image" />
-        <Card.Body className="card-text-container">
-          <Card.Title className="card-title">Card Title</Card.Title>
-          <Card.Text className="card-subtitle">Location</Card.Text>
-          <Card.Text className="card-description">Description...</Card.Text>
-          <Link to="/enquiry" className="card-button">
-            Make an enquiry
-          </Link>
-        </Card.Body>
-      </Card>
+      <Link className="card-detail-link" to="/hotel-detail">
+        <Card className="card">
+          <Card.Img className="card-image" src={image} alt="image" />
+          <Card.Body className="card-text-container">
+            <Card.Title className="card-title">Card Title</Card.Title>
+            <Card.Text className="card-subtitle">Location</Card.Text>
+            <Card.Text className="card-description">Description...</Card.Text>
+            <Link to="/enquiry" className="card-button">
+              Make an enquiry
+            </Link>
+          </Card.Body>
+        </Card>
+      </Link>
     </main>
   );
 }
