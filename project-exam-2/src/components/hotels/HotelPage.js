2eec43a71e833bc3a230229d158342aebe9d6f97
MinhBuiCong <62849019+MinhBuiCong@users.noreply.github.com>
2022-05-16 14:38:25 +0200

    update

--- project-exam-2/src/components/hotels/HotelPage.js
+++ project-exam-2/src/components/hotels/HotelPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import HotelCard from "../card/HotelCard";
 import Search from "../search/Search";
 import Button from "react-bootstrap/Button";
@@ -6,14 +6,26 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFilter } from "@fortawesome/free-solid-svg-icons";
 
 function HotelPage() {
+  const [filterOpen, setFilterOpen] = useState(false);
+
   return (
     <main className="hotel-page-container">
       <h1 className="title">Hotels</h1>
       <div className="filter-container">
         <Search />
-        <Button className="filter-button">
+        <Button
+          className="filter-button"
+          onClick={() => setFilterOpen(!filterOpen)}
+        >
           <FontAwesomeIcon icon={faFilter} />
         </Button>
+      </div>
+      <div className={filterOpen ? "filter-choice-visible" : "filter-choice"}>
+        <Button className="brand-alternatives-button">brand</Button>
+        <Button className="brand-alternatives-button">brand</Button>
+        <Button className="brand-alternatives-button">brand</Button>
+        <Button className="brand-alternatives-button">brand</Button>
+        <Button className="brand-alternatives-button">brand</Button>
       </div>
       <HotelCard />
     </main>
