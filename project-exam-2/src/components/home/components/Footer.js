5b721131d3b24ea75854b0b3b4cef439d90fc1d4
MinhBuiCong <62849019+MinhBuiCong@users.noreply.github.com>
2022-05-20 22:17:58 +0200

    Update Footer.js

--- project-exam-2/src/components/home/components/Footer.js
+++ project-exam-2/src/components/home/components/Footer.js
@@ -11,15 +11,6 @@ function Footer() {
         <Link className="footer-links" to="/contact">
           <h4>Contact</h4>
         </Link>
-        <Link className="footer-links" to="/login">
-          <h4>Social Media</h4>
-          <i className="fa fa-facebook"></i>
-          <i className="fa fa-instagram"></i>
-          <i className="fa fa-snapchat"></i>
-        </Link>
-        <Link className="footer-links" to="/newsletter">
-          <h4>Newsletter</h4>
-        </Link>
       </div>
       <div className="copyright-container">
         <p>2022 Minh Cong Bui Home Studio. All Rights Reserved.</p>
