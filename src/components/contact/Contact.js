ace5c67e5477ca788f6b8b3aeb76eaf5fa5fd242
MinhBuiCong <62849019+MinhBuiCong@users.noreply.github.com>
2022-05-27 11:11:40 +0200

    added submit form

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { postContact } from "../../constants/api";
 
 function ContactPage() {
+  const [submitted, setsubmitted] = useState(false);
   const handleOnClick = (event) => {
     event.preventDefault();
     postContact({
@@ -15,50 +17,61 @@ function ContactPage() {
     })
       .then()
       .catch((e) => console.error(e));
-    window.location.reload();
+    setsubmitted(true);
   };
 
   return (
     <main className="contact-container">
       <h1 className="title">Contact Holidaze</h1>
       <Form className="form-container" onSubmit={handleOnClick}>
-        <div className="form-row">
-          <Form.Group className="group-input" controlId="name">
-            <Form.Label className="label">Full name</Form.Label>
-            <Form.Control
-              className="input"
-              type="text"
-              placeholder="John"
-              name="name"
-              required
-            />
-          </Form.Group>
-        </div>
-        <div className="form-row">
-          <Form.Group className="group-input" controlId="email">
-            <Form.Label className="label">Email</Form.Label>
-            <Form.Control
-              className="input"
-              type="email"
-              placeholder="John@doe.com"
-              name="email"
-              required
-            />
-          </Form.Group>
-        </div>
-        <Form.Group className="group-input" controlId="message">
-          <Form.Label className="label">Message</Form.Label>
-          <Form.Control
-            className="input"
-            as="textarea"
-            name="message"
-            rows={3}
-            required
-          />
-        </Form.Group>
-        <Button type="submit" className="submit-button">
-          Submit
-        </Button>
+        {!submitted ? (
+          <>
+            <div className="form-row">
+              <Form.Group className="group-input" controlId="name">
+                <Form.Label className="label">Full name</Form.Label>
+                <Form.Control
+                  className="input"
+                  type="text"
+                  placeholder="John"
+                  name="name"
+                  required
+                />
+              </Form.Group>
+            </div>
+            <div className="form-row">
+              <Form.Group className="group-input" controlId="email">
+                <Form.Label className="label">Email</Form.Label>
+                <Form.Control
+                  className="input"
+                  type="email"
+                  placeholder="John@doe.com"
+                  name="email"
+                  required
+                />
+              </Form.Group>
+            </div>
+            <Form.Group className="group-input" controlId="message">
+              <Form.Label className="label">Message</Form.Label>
+              <Form.Control
+                className="input"
+                as="textarea"
+                name="message"
+                rows={3}
+                required
+              />
+            </Form.Group>
+            <Button type="submit" className="submit-button">
+              Submit
+            </Button>
+          </>
+        ) : (
+          <div className="form-submit-container">
+            <h2 className="form-submit-title">Form is submitted</h2>
+            <Link className="link-submit" to="/">
+              <Button className="home-page-button">Homepage</Button>
+            </Link>
+          </div>
+        )}
       </Form>
     </main>
   );
--- src/components/enquiry/enquiry.js
+++ src/components/enquiry/enquiry.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
+import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import { postEnquiry } from "../../constants/api";
 
 function EnquiryPage() {
+  const [submitted, setsubmitted] = useState(false);
   const handleOnClick = (event) => {
     event.preventDefault();
     postEnquiry({
@@ -15,50 +17,61 @@ function EnquiryPage() {
     })
       .then()
       .catch((e) => console.error(e));
-    window.location.reload();
+    setsubmitted(true);
   };
 
   return (
     <main className="enquiry-container">
       <h1 className="title">Enquiry page</h1>
       <Form className="form-container" onSubmit={handleOnClick}>
-        <div className="form-row">
-          <Form.Group className="group-input" controlId="first-name">
-            <Form.Label className="label">Full name</Form.Label>
-            <Form.Control
-              className="input"
-              type="text"
-              placeholder="John"
-              required
-              name="name"
-            />
-          </Form.Group>
-        </div>
-        <div className="form-row">
-          <Form.Group className="group-input" controlId="email">
-            <Form.Label className="label">Email address</Form.Label>
-            <Form.Control
-              className="input"
-              type="email"
-              placeholder="John@doe.com"
-              required
-              name="email"
-            />
-          </Form.Group>
-        </div>
-        <Form.Group className="group-input" controlId="message">
-          <Form.Label className="label">Message</Form.Label>
-          <Form.Control
-            className="input"
-            as="textarea"
-            rows={3}
-            required
-            name="message"
-          />
-        </Form.Group>
-        <Button type="submit" className="submit-button">
-          Submit
-        </Button>
+        {!submitted ? (
+          <>
+            <div className="form-row">
+              <Form.Group className="group-input" controlId="first-name">
+                <Form.Label className="label">Full name</Form.Label>
+                <Form.Control
+                  className="input"
+                  type="text"
+                  placeholder="John"
+                  required
+                  name="name"
+                />
+              </Form.Group>
+            </div>
+            <div className="form-row">
+              <Form.Group className="group-input" controlId="email">
+                <Form.Label className="label">Email address</Form.Label>
+                <Form.Control
+                  className="input"
+                  type="email"
+                  placeholder="John@doe.com"
+                  required
+                  name="email"
+                />
+              </Form.Group>
+            </div>
+            <Form.Group className="group-input" controlId="message">
+              <Form.Label className="label">Message</Form.Label>
+              <Form.Control
+                className="input"
+                as="textarea"
+                rows={3}
+                required
+                name="message"
+              />
+            </Form.Group>
+            <Button type="submit" className="submit-button">
+              Submit
+            </Button>
+          </>
+        ) : (
+          <div className="form-submit-container">
+            <h2 className="form-submit-title">Form is submitted</h2>
+            <Link className="link-submit" to="/">
+              <Button className="home-page-button">Homepage</Button>
+            </Link>
+          </div>
+        )}
       </Form>
     </main>
   );
